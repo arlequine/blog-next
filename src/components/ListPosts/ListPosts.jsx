e6889ca4101dbe662fa1b794dc792d8df7d06f83
Arlequin <arlequine666.ah@gmail.com>
2024-07-14 22:25:28 -0600

    feat(Listpos):add key prop

--- src/components/ListPosts/ListPosts.jsx
+++ src/components/ListPosts/ListPosts.jsx
@@ -7,7 +7,7 @@ const ListPosts = ({data}) => {
     <div className="w-full flex flex-wrap justify-center" >
       {
         data && data.map(item => (
-          <Link href={`post/${item.slug}`} className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 m-4">
+          <Link key={item.id} href={`post/${item.slug}`} className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 m-4">
             <CardPost dataCard={item} />
           </Link>
         ))
